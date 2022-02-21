ebf23979ebdc0e4b9109863a0c33f4a780b1ff42
shanthiniguru <96715415+shanthiniguru@users.noreply.github.com>
2022-02-21 12:24:48 +0530

    Update FindIntersectionOfAnArray.java

--- FindIntersectionOfAnArray.java
+++ FindIntersectionOfAnArray.java
@@ -7,7 +7,7 @@ public class FindIntersectionOfAnArray {
 	public static void main(String[] args) {
 		int[] fir = {3,2,11,4,6,7};	
 		int[] sec = {1,2,8,4,9,7};
-		System.out.println("The intersection of an array:/n ");
+		System.out.println("The intersection of an array: ");
 		for (int i = 0; i < fir.length; i++) {
 				for (int j = 0; j < sec.length; j++) {
 					if (fir[i] == sec[j]) {
